f5d3cfc83095dc8d6cc7e84905cee6a300fda0f8
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2026-08-26 18:20:28 -0500

    Forzar IPv4 en JVM (preferIPv4Stack) para conectar Supabase

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,5 @@
 set -e
 PORT="${PORT:-8080}"
 sed -i "s/Connector port=\"8080\"/Connector port=\"${PORT}\"/" /usr/local/tomcat/conf/server.xml
+export JAVA_OPTS="${JAVA_OPTS} -Djava.net.preferIPv4Stack=true"
 exec catalina.sh run
